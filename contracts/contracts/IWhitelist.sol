1716c6a39998e147f9d38390c0758a95b5d820a8
ShadowSong <ShadowSong.dev@gmail.com>
2026-04-29 18:01:29 +0800

    add dependence:@openzeppelin/contracts.delete IWhitelist.sol

--- contracts/contracts/IWhitelist.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-interface IWhitelist {
-    function owner() external view returns (address);
-    function isWhitelisted(address addr) external view returns (bool);
-}
